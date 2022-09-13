3dbac868f21e0434d42c8aeb353b480bfe127418
Ucrash <gducrash@gmail.com>
2022-09-13 09:32:04 +0300

    add event definitions

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import ScriptRunner from './src/classes/scriptRunner';
 import definitions  from './src/data/definitions.json';
+import events       from './src/data/events.json';
 import windowModify from './src/data/windowModify.json';
 
-export { ScriptRunner, definitions, windowModify }
+export { ScriptRunner, definitions, events, windowModify }
